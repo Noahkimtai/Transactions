06579a2abf19ed0f61de6d7fca7bfef902dbd9f1
Noahkimtai <noahkimtai24@gmail.com>
2022-10-04 02:11:32 +0300

    state added to ProductList and passed as props to Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Product(){
+function Product({product}){
     return (
         <div>
-
+            {product.map(prod => {return(<img key={prod.id} src={prod.image}></img>)})}
         </div>
     );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,19 @@
 import React, {useState,useEffect} from "react";
+import Product from "./Product";
 
 function ProductList(){
     const [products, setProducts] = useState([]);
     useEffect(()=>{
         fetch('https://fakestoreapi.com/products').then(res => res.json()).
-        then(data =>setProducts(data));})
-    
+        then(data =>setProducts(data));
+    },[])
+
+    /* const product = products.map((product) =>{
+        <Product product={product}/>
+    }) */
     return(
         <div>
-            <p>{products.length}</p>
+            <Product product={products}/>
         </div>
     );
 }
