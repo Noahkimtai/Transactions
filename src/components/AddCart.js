f2930e718409b996207338b7ef05ac3241deccb2
Noahkimtai <noahkimtai24@gmail.com>
2022-10-06 17:51:09 +0300

    description added to items on the cart

--- src/components/AddCart.js
+++ src/components/AddCart.js
@@ -5,10 +5,11 @@ function AddCart({cart, handleAddCart}){
         <div className="wrapper">
             <h2>My Cart</h2>
             {cart.map(prod => {return(
-            <>
+            <div>
                 <img key={prod.id} src={prod.image} alt={prod.id} className="img-responsive"></img> 
-                <h3>Price: {prod.price} <span>Rating:{prod.rating.rate}</span></h3> 
-            </>
+                <h5>Price: {prod.price} <span>Rating:{prod.rating.rate}</span></h5>
+                <p>{prod.title} {prod.description}</p>
+            </div>
         )})}
         </div>
     );
