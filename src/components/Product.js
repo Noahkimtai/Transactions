e0a74a4689df921114274d6d0926aff7ddbf8165
Noahkimtai <noahkimtai24@gmail.com>
2022-10-06 22:12:37 +0300

    POST method to update the rating when clicked

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Product({product, addToCart}){
     //create state to hold the rating
     const [rating,setRating] = useState(product.rating.rate)
+    const [rateCount, setRateCount] =useState(product.rating.count)
+    
+    // When the rating changes make a post request to the server to change the rating
+/*     useEffect(() =>{
+        product.rating.rate = rating
+        product.rating.count = rateCount
+        fetch(`https://fakestoreapi.com/products/${product.id}`,{
+            method:'PATCH',
+            headers:{'content-Type':'application/json'},
+            body:JSON.stringify(product)
+        }).then(res=>res.json())
+        .then(json=>alert(json.id))
+    },[rating]) */
     return (
     <div>
         <div>
             <img src={product.image} alt={product.id} className="img-responsive"></img>
-            <button className ='star' onClick={(e)=>{setRating(() =>rating+0.1)}}>★</button>
+            <button className ='star' onClick={(e)=>{setRating((rating) =>rating+0.1);
+            setRateCount((rateCount) =>rateCount+1)}}>★</button>
             <button onClick={()=>addToCart(product.id)}>Buy</button> 
             <p>Ksh: {product.price} Rating:{rating}</p> 
         </div>
