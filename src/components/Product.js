13593f816265cffdad71456744d3e4e9cbee2b43
Noahkimtai <noahkimtai24@gmail.com>
2022-10-06 19:13:31 +0300

    typo on Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 
-function Product({product, addToCart}){
+function Product({product}){
     //create state to hold the rating
     const [rating,setRating] = useState(product.rating.rate)
     return (
     <div className="wrapper">
-        <div className="section">
+        <div>
             <img src={product.image} alt={product.id} className="img-responsive"></img>
             <button className ='star' onClick={(e)=>{setRating(() =>rating+0.1)}}>★</button>
-            <button onClick={() =>addToCart(product.id)}>Buy</button> 
+            <button>Buy</button> 
             <p>Price: {product.price} <span>Rating:{rating}</span></p> 
         </div>
     </div>
